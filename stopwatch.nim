d5482d629fe8845d1ed7277dbe8f22ddce07a4bb
Benjamin N. Summerton <def.pri.pub@gmail.com>
2016-12-03 15:26:12 -0500

    Working on adding laps

--- stopwatch.nim
+++ stopwatch.nim
@@ -4,47 +4,65 @@ include system/timers
 type
   Stopwatch* = object
     running: bool
-    start: Nanos 
-    stop: Nanos 
+    startTicks: Nanos 
+    stop: Nanos
+    laps: seq[Nanos]
+
+
+# Function prototypes
+proc newStopwatch*(): Stopwatch
+proc running*(sw: var Stopwatch): bool {.inline.}
+proc start*(sw: var Stopwatch) {.inline.}
+proc stop*(sw: var Stopwatch) {.inline.}
+proc nsecs*(sw: var Stopwatch): int64 {.inline.}
+#proc usecs*(sw: var Stopwatch): int64 {.inline.}
+#proc msecs*(sw var Stopwatch): int64 {.inline.}
+proc secs*(sw: var Stopwatch): float {.inline.}
 
 
 # TODO document
 proc newStopwatch*(): Stopwatch =
   result = Stopwatch(
     running: false,
-    start: 0,
-    stop: 0
+    stop: 0,
+    laps: @[]
   )
 
 
 # TODO clone/copy constructor
 
 
-proc running*(sw: var Stopwatch): bool {.inline.} =
+proc running*(sw: var Stopwatch): bool =
   return sw.running
 
 
-proc start*(sw: var Stopwatch) {.inline.} =
-  sw.start = getTicks().Nanos
+proc start*(sw: var Stopwatch) =
+  # If we are already running, ignore
+  if sw.running:
+    return
+
+  # 
+
+  sw.startTicks = getTicks().Nanos
 
 
-proc stop*(sw: var Stopwatch) {.inline.} =
+proc stop*(sw: var Stopwatch) =
   sw.stop = getTicks().Nanos
 
 
-proc nsecs*(sw: var Stopwatch): int64 {.inline.} =
-  return (sw.stop - sw.start).int64
+proc nsecs*(sw: var Stopwatch): int64 =
+  return (sw.stop - sw.startTicks).int64
 
 
-#proc usecs*(sw: var Stopwatch): int64 {.inline.} =
+#proc usecs*(sw: var Stopwatch): int64 =
 #  return sw.nsecs / 1_000.int64
 #
 #
-#proc msecs*(sw var Stopwatch): int64 {.inline.} =
+#proc msecs*(sw var Stopwatch): int64 =
 #  return sw.nsecs / 1_000_000.int64
 
 
-proc secs*(sw: var Stopwatch): float {.inline.} =
+proc secs*(sw: var Stopwatch): float =
   return sw.nsecs.float / 1_000_000_000.0
 
 
